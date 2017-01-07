50c9e50ae0f46ab91f36cd340b7cd52cb32533c3
KasamR <fun.b1013112@gmail.com>
2017-01-07 17:26:30 +0900

    おつりの計算などの追加

--- automat.cpp
+++ automat.cpp
@@ -6,7 +6,7 @@
 #define NOT_FOUND -2
 using namespace std;
 
-class Drink {
+class Drink { //飲み物クラス
 private:
 	string name = "";
 	int money = 0;
@@ -15,6 +15,7 @@ public:
 	Drink(int m, string n);
 	string getName() { return name; }
 	int getMoney() { return money; }
+	int calcChange(int sum); //残金の計算
 };
 
 Drink::Drink(int m, string n) {
@@ -22,7 +23,11 @@ Drink::Drink(int m, string n) {
 	name = n;
 }
 
+int Drink::calcChange(int sum) {
+	return sum - money; //引数から自分の分の代金を引く
+}
 
+//csvから商品のお金と名前を読み取ってvectorを返す関数
 vector<Drink> read() {
 	ifstream ifs("items.csv");
 	int i = 0;
@@ -47,14 +52,31 @@ bool MoneyCheck(int money) {
 //買おうとしたときの確認をする関数
 int BuyCheck(vector<Drink> items, int sum, string input) {
 	for (int i = 0; i < items.size(); i++) {
-		if (items[i].getName() == input && 0 <= sum - items[i].getMoney()) { //ちゃんと買えた時(リストの何番目かを返す)
-			return i;
+		if (items[i].getName() == input && 0 <= sum - items[i].getMoney()) { //ちゃんと買えた時
+			return i; //リストの何番目かを返す
 		}
 		if (items[i].getName() == input && sum - items[i].getMoney() < 0) { //お金が足りないとき
-			return NO_MONEY;
+			return NO_MONEY; //-1を返す
 		}
 	}
-	return NOT_FOUND; //リストに名前がない
+	return NOT_FOUND; //リストに名前がない(-2を返す)
+}
+
+//おつりの計算と表示をする関数
+void changeMoney(int sum) {
+	vector<int> m = { 1000, 500, 100, 50, 10 };
+	if (sum == 0) { //引数が0の時はちょうどの時
+		cout << "釣銭はありません" << endl;
+	} else { //そうでないときは計算をする
+		cout << "おつりは" << sum << "円で，釣銭は ";
+		for (int i = 0; i < m.size(); i++) {
+			if (0 < sum / m[i]) { //"~円が0枚"という表示を避けるための処理
+				cout << m[i] << "円が" << sum / m[i] << "枚 ";
+				sum = sum - (sum / m[i])*m[i];
+			}
+		}
+		cout << "です" << endl;
+	}
 }
 
 int main() {
@@ -90,13 +112,13 @@ int main() {
 					break;
 				default:
 					cout << "ゴトン！" << items[bc].getName() << "を購入しました！" << endl;
-					sum = sum - items[bc].getMoney(); //残金の計算
+					sum = items[bc].calcChange(sum); //残金の計算
 					break;
 				}
 			}
 		}
 		printf("\n");
 	} while (loop);
-	cout << "おつりは" << sum << "円です" << endl;
+	changeMoney(sum); //おつりの表示と計算
 	cout << "ご利用ありがとうございました" << endl;
 }
